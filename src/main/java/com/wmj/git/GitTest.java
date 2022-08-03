d02b1729f8b507247cfa47f72efe561eb8ee746f
wmj <1478147296@qq.com>
2022-08-03 17:30:50 +0800

    first commit

---
+++ src/main/java/com/wmj/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wmj.git;
+
+/**
+ * @auther 王明军
+ * @create 2022-08-03 17:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
